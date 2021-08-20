6200e697fc11bb573467ab3e4245f22f26fac007
azu <azuciao@gmail.com>
2021-08-20 10:01:02 +0900

    fix: console.table on firefox

--- app/scripts/contentScript.ts
+++ app/scripts/contentScript.ts
@@ -5,8 +5,18 @@ onMessage("content-script:log-lint-messages", async (message) => {
     const { data, sender } = message;
     if (isInternalEndpoint(sender)) {
         const messages = data as SecretLintMessage[];
-        console.group(`Found ${messages.length} secrets. For more details see Secretlint panel.`);
-        console.table(messages, ["ruleId", "message", "url"]);
+        // FIXME: Firefox does not preserve order, manual sorts
+        const sortedMessages: SecretLintMessage[] = messages.map((message) => {
+            return {
+                ...message,
+                "1.ruleId": message.ruleId,
+                "2.message": message.message,
+                "3.url": message.url,
+                "4.sliceContent": message.sliceContent
+            };
+        });
+        console.group(`Found ${sortedMessages.length} secrets. For more details see Secretlint panel.`);
+        console.table(sortedMessages, ["1.ruleId", "2.message", "3.url", "4.sliceContent"]);
         console.groupEnd();
     }
     return {};
